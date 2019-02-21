3bb917c8d84afb4491981c2945c9c0ce99381f43
John Freund <jfreund91@gmail.com>
2019-02-21 10:55:51 -0500

    Added DAL interfaces for models.

---
+++ Capstone/DAL/ICampgroundDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Models;
+namespace Capstone.DAL
+{
+    interface ICampgroundDAO
+    {
+        /// <summary>
+        /// Gets an individual campground.
+        /// </summary>
+        /// <param name="campgroundId"></param>
+        /// <returns></returns>
+        Campground GetCampground(int campgroundId);
+        /// <summary>
+        /// Returns a list of all campgrounds in a park.
+        /// </summary>
+        /// <param name="parkId"></param>
+        /// <returns></returns>
+        IList<Campground> GetAllCampgrounds(int parkId);
+    }
+}
--- Capstone/DAL/IParkDAO.cs
+++ Capstone/DAL/IParkDAO.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Capstone.Models
+namespace Capstone.DAL
 {
-    class Class1
+    interface Interface1
     {
     }
 }
