21490e7712b68c510953c51887cef30a76898ef5
Ken Bohac <bohac91@gmail.com>
2019-02-21 11:47:22 -0500

    added ParkSqlDAO

---
+++ Capstone/DAL/ParkSqlDAO.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Capstone.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.DAL
+{
+    public class ParkSqlDAO : IParkDAO
+    {
+        private string connectionString;
+        
+            public ParkSqlDAO (string dbconnection)
+        {
+            connectionString = dbconnection; 
+        }
+
+        public IList<Park> GetAllParks()
+        {
+            List<Park> parks = new List<Park>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM park", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Park park = ConvertReaderToPark(reader);
+                        parks.Add(park);
+
+                    } 
+                }
+                
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("An error has occurred while reading Parks");
+                Console.WriteLine(ex.Message);
+            }
+            return parks;
+        }
+
+        private Park ConvertReaderToPark(SqlDataReader reader)
+        {
+            Park park = new Park();
+            park.ParkId = Convert.ToInt32(reader["park_id"]);
+            park.Name = Convert.ToString(reader["name"]);
+            park.Location = Convert.ToString(reader["location"]);
+            park.EstablishDate = Convert.ToDateTime(reader["establish_date"]);
+            park.Area = Convert.ToInt32(reader["area"]);
+            park.Visitors = Convert.ToInt32(reader["visitors"]);
+            park.Description = Convert.ToString(reader["description"]);
+
+            return park;
+
+        }
+        public Park GetPark(int parkId)
+        {
+            Park park = new Park();
+            try
+            {
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM park WHERE park_id =@parkId;", conn);
+                    cmd.Parameters.AddWithValue("@parkId", parkId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        park = ConvertReaderToPark(reader);
+                    }
+
+
+                }
+
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("An error occured fetching your park.");
+                Console.WriteLine(ex.Message);
+            }
+             return park;
+        }
+       
+    }
+}
